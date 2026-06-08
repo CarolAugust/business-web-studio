0b11ea93b56aecf75cdff2e02008c15233352831
Caroline Augusto <126285992+CarolAugust@users.noreply.github.com>
2026-06-08 16:49:08 -0300

    Ajustado o nome

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Carol Web Studio",
+  title: "CAS Web SolutionsStudio",
   description: "Sites simples e profissionais para pequenos negócios.",
 };
 
